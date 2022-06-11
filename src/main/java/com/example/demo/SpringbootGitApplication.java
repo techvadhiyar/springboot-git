237760c112ed64a14d01cd255186a51353ec791d
cpccharry <harry.subramaniam@cpcc.edu>
2022-06-11 17:39:32 -0400

    Initial release.

--- src/main/java/com/example/demo/SpringbootGitApplication.java
+++ src/main/java/com/example/demo/SpringbootGitApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Adding some documentation....
 @SpringBootApplication
 public class SpringbootGitApplication {
 
